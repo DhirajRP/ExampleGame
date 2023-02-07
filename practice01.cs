331d188d637373b043ce7b36f7e72415ad931541
DHIRAJ PATIL <ardhirajrp@gmail.com>
2023-02-08 00:02:27 +0530

    Add files via upload

---
+++ practice01.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic
+using UnityEngine;
+
+public class practice01 : MonoBehaviour
+
+/*
+if else
+for loops
+while loops
+array : collection of values
+list: 
+switch statements
+*/
+
+
+// data types
+/*
+int
+float/ double
+char / string
+bool - boolean
+*/
+
+
+
+
+{
+
+    public List<string> vegetableNames;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
